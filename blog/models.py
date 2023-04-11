fdd74ce57d9abc959b107bc175a471b6dbacd42e
Salimul hasan <shbappi07@gmail.com>
2023-04-12 01:48:09 +0600

    backup before migration

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
 # Create your models here.
+
+class Blog(models.Model):
+    blog_name = models.CharField(max_length=200)
+    details = models.CharField(max_length=200)
+
+    def __str__(self):
+        self.blog_name
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    slug = models.SlugField(unique=True)
+    description = models.TextField()
+    featured_image = models.ImageField(upload_to='uploads/')
+    tags = models.CharField(max_length=200)
+    featured = models.BooleanField(default=False)
+    pub_date = models.DateTimeField(auto_now=True)
+    mod_date = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        self.title
+
+
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,6 @@
 from django.shortcuts import render
 
 # Create your views here.
+def home_view(request):
+    contex = {}
+    return render(request, "blog/index.html", contex)
